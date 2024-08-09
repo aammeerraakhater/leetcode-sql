a68582793ff3180d3a411feb0f69c58f92d70c95
Amira <65508978+aammeerraakhater@users.noreply.github.com>
2024-08-09 19:33:07 +0300

    [1280] [Time Beats: 69.41%] - LeetPush

---
+++ 1280-Students-and-Examinations.sql
@@ -0,0 +1,1 @@
+select s.student_id, student_name, su.subject_name, count(e.subject_name) as attended_exams from Students s join Subjects su left join Examinations e on s.student_id = e.student_id and su.subject_name = e.subject_name group by s.student_id, su.subject_name order by s.student_id, su.subject_name;
